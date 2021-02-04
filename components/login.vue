fa4a416eca94778f2b2bfd3c3f7d175d47fd798d
Ayoub Bouchedoub <abouched@capgemini.com>
2021-02-04 11:26:50 +0100

    Prepare the Auth

---
+++ components/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="userLogin">
+      <div>
+        <label>Email</label>
+        <input type="text" v-model="login.email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" v-model="login.password" />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async userLogin() {
+      try {
+        let response = await this.$auth.loginWith('local', {data: {"Email": this.login.email, "Password": this.login.password}})
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
